072e345cea55519deae89c9767f233b1a2f052d4
Satish Samusakhare <samusakharesatish@gmail.com>
2024-08-13 13:27:00 +0530

    add

---
+++ pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern{
+    public static void main(String args[])
+{
+    int n=4;
+    int m=5;
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            System.out.println("*");
+        }
+    }
+}
+}
