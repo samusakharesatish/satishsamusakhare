be6121296115a88b3d75c43c7074dfbdcea03aa0
Satish Samusakhare <samusakharesatish@gmail.com>
2025-03-17 22:41:35 +0530

    add program

--- function.java
+++ function.java
@@ -1,18 +1,37 @@
 package satishsamusakhare;
 import java.util.Scanner;
 
+// public class function{
+
+//     public static void myNamePrint(String name) {
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+
+//         myNamePrint(name);      //call the function
+        
+//     }
+    
+// }
+
 public class function{
 
-    public static void myNamePrint(String name) {
-        System.out.println(name);
-        return;
+    public static int printValue(int a, int b){
+        int sum = a + b;
+        return sum;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
 
-        myNamePrint(name);      //call the function
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
+        int sum = printValue(a,b);
+        System.out.println(sum);
+
+
     }
-    
 }
